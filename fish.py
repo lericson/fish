2406234b80bd42a54fe812dbb46c412ee2adfad1
lericson <ludvig@lericson.se>
2010-05-18 19:37:09 +0200

    Break out the progressing swimming fish
    This should separate responsibilities even further

--- fish.py
+++ fish.py
@@ -18,48 +18,75 @@
 
 import sys
 import time
-from itertools import count
+from itertools import cycle, count
+
+ANSI_LINE_CLEAR = "\x1b[2K\r"
 
 class SwimFishBase(object):
-    def __init__(self, velocity=10, world_length=79, outfile=sys.stderr, total=None):
-        if total > 0:
-            pad = len(str(total))*2
-            pad += 6
-            world_length = world_length - pad
+    def __init__(self, velocity=10, world_length=79, outfile=sys.stderr):
         self.worldstep = self.make_worldstepper()
         self.velocity = velocity
         self.world_length = world_length
         self.outfile = outfile
         self.last_hash = 0
-        self.total = total
 
-    def animate(self, outfile=None, force=False, amount=''):
+    def animate(self, outfile=None, force=False):
         step = self.worldstep.next()
         # Refit the world so that we can move along an axis and not worry about
         # overflowing
         actual_length = self.world_length - self.own_length
-        if amount and self.total:
-            amount_string = '%s%% %s/%s' % \
-                (str(int(round(amount/float(self.total)*100))).rjust(3), \
-                str(amount).rjust(len(str(self.total))), self.total)
-            pos = (amount/float(self.total))*float(actual_length)
-            pos += actual_length #so it moves left to right
-        else:
-            amount_string = '%s' % amount
-            # As there are two directions we pretend the world is twice as large as
-            # it really is
-            pos = (self.velocity * step) % (actual_length * 2)
+        # As there are two directions we pretend the world is twice as large as
+        # it really is, then handle the overflow
+        pos = (self.velocity * step) % (actual_length * 2)
         reverse = pos < actual_length
         pos = int(round(abs(pos - actual_length), 0))
         fish = self.render(reverse=reverse)
         of = outfile or self.outfile
-        curr_hash = force or hash((of, pos, fish, amount_string))
+        curr_hash = force or hash((of, pos, fish))
         if force or curr_hash != self.last_hash:
-            lead = " " * (pos)
-            trail = " " * (self.world_length - self.own_length - pos)
-            of.write("\x1b[2K\r" + lead + fish + trail + amount_string + "\r")
+            self.print_line(of, pos, fish)
             of.flush()
             self.last_hash = curr_hash
+
+    def print_line(self, of, pos, fish):
+        lead = " " * pos
+        trail = " " * (self.world_length - self.own_length - pos)
+        of.write(ANSI_LINE_CLEAR + lead + fish + trail + "\r")
+
+class ProgressableFishBase(SwimFishBase):
+    def __init__(self, *args, **kwds):
+        total = kwds.pop("total", None)
+        super(ProgressableFishBase, self).__init__(*args, **kwds)
+        if total:
+            # `pad` is the length required for the progress indicator,
+            # It, at its longest, is `100% 123/123`
+            pad = len(str(total)) * 2
+            pad += 6
+            self.world_length -= pad
+        self.total = total
+
+    def animate(self, *args, **kwds):
+        prev_amount = getattr(self, "amount", None)
+        self.amount = kwds.pop("amount", None)
+        if self.amount != prev_amount:
+            kwds["force"] = True
+        return super(ProgressableFishBase, self).animate(*args, **kwds)
+
+    def print_line(self, of, pos, fish):
+        if not self.amount:
+            return super(ProgressableFishBase, self).print_line(of, pos, fish)
+
+        # Get the progress text
+        if self.total:
+            part = self.amount / float(self.total)
+            done_text = str(self.amount).rjust(len(str(self.total)))
+            progress = "%3.d%% %s/%d" % (part * 100, done_text, self.total)
+        else:
+            progress = str(amount)
+
+        lead = " " * pos
+        trail = " " * (self.world_length - self.own_length - pos)
+        of.write(ANSI_LINE_CLEAR + lead + fish + trail + progress + "\r")
 
 class BassLook(SwimFishBase):
     def render(self, reverse=False):
@@ -83,7 +110,7 @@ class SwimFishTimeSync(SwimFishBase):
     def make_worldstepper(cls):
         return iter(time.time, None)
 
-class Fish(SwimFishTimeSync, BassLook):
+class Fish(ProgressableFishBase, SwimFishTimeSync, BassLook):
     """The default swimming fish, the one you very likely want to use.
     See module-level documentation.
     """
