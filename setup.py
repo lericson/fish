1230328d7307fd2b1d3f8a7e1fdf60aa5a708aa1
lericson <ludvig@lericson.se>
2010-05-18 21:37:47 +0200

    Bump version

--- setup.py
+++ setup.py
@@ -3,10 +3,10 @@ from os import path
 
 long_desc = open(path.join(path.dirname(__file__), "README.rst"), "U").read()
 
-setup(name="fish", version="0.4",
+setup(name="fish", version="1.0",
       url="http://sendapatch.se/",
       author="Ludvig Ericson",
       author_email="ludvig@sendapatch.se",
-      description="Animating fishes for progress bars",
+      description="Animating fish (and birds) for progress bars",
       long_description=long_desc,
       py_modules=["fish"])
