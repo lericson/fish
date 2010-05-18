3aef0ee22d573703f172efdab2fbda18d3302ed7
Chris Mulligan <chris.mulligan@gmail.com>
2010-05-18 11:56:25 -0400

    Using curr_hash to force display with an amount, updated variable names to better match existing style.

--- fish.py
+++ fish.py
@@ -21,9 +21,9 @@ import time
 from itertools import count
 
 class SwimFishBase(object):
-    def __init__(self, velocity=10, world_length=79, outfile=sys.stderr, tot=None):
-        if tot > 0:
-            pad = len(str(tot))*2
+    def __init__(self, velocity=10, world_length=79, outfile=sys.stderr, total=None):
+        if total > 0:
+            pad = len(str(total))*2
             pad += 6
             world_length = world_length - pad
         self.worldstep = self.make_worldstepper()
@@ -31,33 +31,33 @@ class SwimFishBase(object):
         self.world_length = world_length
         self.outfile = outfile
         self.last_hash = 0
-        self.tot = tot
+        self.total = total
 
-    def animate(self, outfile=None, force=False, amt=''):
+    def animate(self, outfile=None, force=False, amount=''):
         step = self.worldstep.next()
         # Refit the world so that we can move along an axis and not worry about
         # overflowing
         actual_length = self.world_length - self.own_length
-        if amt and self.tot:
-            amount = '%s%% %s/%s' % \
-                (str(int(round(amt/float(self.tot)*100))).rjust(3), \
-                str(amt).rjust(len(str(self.tot))), self.tot)
-            pos = (amt/float(self.tot))*float(actual_length)
+        if amount and self.total:
+            amount_string = '%s%% %s/%s' % \
+                (str(int(round(amount/float(self.total)*100))).rjust(3), \
+                str(amount).rjust(len(str(self.total))), self.total)
+            pos = (amount/float(self.total))*float(actual_length)
             pos += actual_length #so it moves left to right
         else:
-            amount = '%s' % amt
+            amount_string = '%s' % amount
+            # As there are two directions we pretend the world is twice as large as
+            # it really is
             pos = (self.velocity * step) % (actual_length * 2)
-        # As there are two directions we pretend the world is twice as large as
-        # it really is
         reverse = pos < actual_length
         pos = int(round(abs(pos - actual_length), 0))
         fish = self.render(reverse=reverse)
         of = outfile or self.outfile
-        curr_hash = force or hash((of, pos, fish))
-        if force or amt or curr_hash != self.last_hash:
+        curr_hash = force or hash((of, pos, fish, amount_string))
+        if force or curr_hash != self.last_hash:
             lead = " " * (pos)
             trail = " " * (self.world_length - self.own_length - pos)
-            of.write("\x1b[2K\r" + lead + fish + trail + amount + "\r")
+            of.write("\x1b[2K\r" + lead + fish + trail + amount_string + "\r")
             of.flush()
             self.last_hash = curr_hash
 
@@ -94,9 +94,9 @@ animate = default_fish.animate
 if __name__ == "__main__":
     try:
         total = int(sys.argv[1])
-        f = Fish(tot=total)
+        f = Fish(total=total)
         for i in range(total):
-            f.animate(amt=i+1)
+            f.animate(amount=i+1)
             time.sleep(0.1)
     except IndexError: 
         while True:
